e8168e3d6a1a52e3efd73cdaf48aaa35975ddcdb
Angeth-Herjok <angethherjok@gmail.com>
2023-02-15 15:46:39 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+fun main() {
+    difference(9,2)
+    division(9,3)
+    var greet="Hey "
+    var name= "John"
+    println(greet + name)
+    product(9,8)
+    val citizenship:Boolean=true
+    println(citizenship)
+
+
+
+}
+fun difference(num1:Int,num2:Int){
+    println(num1 - num2)
+}
+fun division(num3:Int,num4:Int){
+    var division=num3/num4
+    println(division)
+}
+fun product(k:Int,h:Int){
+    var product=k*h
+    println(product)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
